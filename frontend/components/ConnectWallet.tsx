0bfca9f20a55df48fc91c8dd50e8dcbdd2ca3c1e
Andres Torres <dualf.business@gmail.com>
2023-10-13 13:34:47 -0400

    show the connect wallet just when is not connected

--- frontend/components/ConnectWallet.tsx
+++ frontend/components/ConnectWallet.tsx
@@ -7,11 +7,19 @@ const ConnectWallet = (props) => {
         <Container className={`flex w-full flex-col mt-4 ${
             props.align === "left" ? "" : "items-center justify-center text-center"
         }`}>
+           {/** show the connect wallet button just if is not connect **/}
+           {!props.address &&
             <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5"
                 onClick={props.handleClick}
             >
               Connect Wallet
             </Link>
+           } 
+
+            <div className="ml-2 flex flex-col">
+                <div className="leading-snug text-xs text-gray-600">{props.address}</div>
+                <div className="leading-snug text-sm text-gray-600">{props.balance}</div>
+            </div>
         </Container>
     );
 }
@@ -23,3 +31,4 @@ ConnectWallet.propTypes = {
 
 
 export default ConnectWallet;
+
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -2,15 +2,9 @@ import Link from "next/link";
 import ThemeChanger from "./DarkSwitch";
 import Image from "next/image"
 import { Disclosure } from "@headlessui/react";
+import PropTypes from 'prop-types';
 
-const Navbar = () => {
-  const navigation = [
-    "Product",
-    "Features",
-    "Pricing",
-    "Company",
-    "Blog",
-  ];
+const Navbar = (props) => {
 
   return (
     <div className="w-full">
@@ -47,14 +41,13 @@ const Navbar = () => {
 
                 <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
                   <>
-                    {navigation.map((item, index) => (
+                    {props.items.map((item, index) => (
                       <Link key={index} href="/" className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none">
                           {item}
                       </Link>
                     ))}
-                    <Link href="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">         
-                        Connect Wallet
-                    </Link>
+
+                    {props.rightContent} 
                   </>
                 </Disclosure.Panel>
               </div>
@@ -65,7 +58,7 @@ const Navbar = () => {
         {/* menu  */}
         <div className="hidden text-center lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
-            {navigation.map((menu, index) => (
+            {props.items.map((menu, index) => (
               <li className="mr-3 nav__item" key={index}>
                 <Link href="/" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
                     {menu}
@@ -76,15 +69,18 @@ const Navbar = () => {
         </div>
 
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
-          <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
-              Connect Wallet
-          </Link>
-
-          <ThemeChanger />
+            {props.rightContent} 
+            <ThemeChanger />
         </div>
       </nav>
     </div>
   );
 }
+
+Navbar.propTypes = {
+    /** The items contains the names to show in the navbar **/
+    items : PropTypes.arrayOf(PropTypes.string),
+};
+
 
 export default Navbar;
