71cc7c53fbc78eeeefa7a289bb2b0e26a7f742e6
Andres Torres <dualf.business@gmail.com>
2023-10-13 23:29:42 -0400

    linter

--- frontend/components/SectionTitle.tsx
+++ frontend/components/SectionTitle.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Container from "./Container";
 
-const SectionTitle = (props) => {
+const SectionTitle = (props:any) => {
   return (
     <Container
       className={`flex w-full flex-col mt-4 ${
