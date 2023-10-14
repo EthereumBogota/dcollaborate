1b47fb4b9b23892ed1f02e480e345b4477568b96
Andres Torres <dualf.business@gmail.com>
2023-10-13 23:50:43 -0400

    linter

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,8 @@
+import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
 
-function MyApp({ Component:any, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
