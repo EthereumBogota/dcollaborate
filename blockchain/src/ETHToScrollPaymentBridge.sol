ebabfc59b872b66a2844f4914023d9f4d9f5194b
andrestorreseth <146355756+andrestorreseth@users.noreply.github.com>
2023-10-20 19:19:23 -0500

    Fix foundry workflow (#13)
    * fix

--- blockchain/src/ETHToScrollPaymentBridge.sol
+++ blockchain/src/ETHToScrollPaymentBridge.sol
@@ -10,7 +10,7 @@ contract ETHToScrollPaymentBridge {
 
     // @notice this function will deposit ETH from L1 to L2
     // @param to the address of recipient's account on L2.
-    // @param gasLimit to process the transaction
+    // @param gasLimit to process the transaction 
     function depositETHToScroll(address to, uint gasLimit) public payable {
         IL1ETHGateway(to).depositETH(msg.sender, msg.value, gasLimit);
     }
