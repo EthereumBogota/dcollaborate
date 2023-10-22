1b31062edad1abcc1d3c2f1adcb90d81f37657af
Andres Torres <dualf.business@gmail.com>
2023-10-22 05:38:41 -0400

    fix marging top buttons

--- frontend/components/Banner.tsx
+++ frontend/components/Banner.tsx
@@ -7,11 +7,12 @@ const Banner = () => {
         <div className="flex items-center w-full lg:w-1/2">
 
           <div className="max-w-2xl mb-8">
+            
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
                 Your reputation now counts to take loans on WEB3
             </h1>
 
-            <div className=" sm:space-x-4 sm:flex-row">
+            <div className="mt-6 sm:space-x-4 sm:flex-row">
               <a
                 href=""
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
